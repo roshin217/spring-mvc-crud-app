cae0d0a30616cf4275817f530ed3628447b06a14
roshin217 <fry0402@gmail.com>
2024-03-12 21:30:50 +0500

    Initial commit

--- src/main/java/web/controllers/UserController.java
+++ src/main/java/web/controllers/UserController.java
@@ -1,6 +1,5 @@
 package web.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -15,7 +14,6 @@ public class UserController {
 
     private final UserService userService;
 
-    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
