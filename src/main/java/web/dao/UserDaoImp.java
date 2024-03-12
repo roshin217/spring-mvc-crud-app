1cf305de6cce95b6ee52b2ed0d67bfab1457ad34
roshin217 <fry0402@gmail.com>
2024-03-12 21:28:26 +0500

    Initial commit

--- src/main/java/web/dao/UserDaoImp.java
+++ src/main/java/web/dao/UserDaoImp.java
@@ -12,31 +12,26 @@ public class UserDaoImp implements UserDao {
     @PersistenceContext
     private EntityManager entityManager;
 
-    @Transactional(readOnly = true)
     @Override
     public List<User> getAllUsers() {
         return entityManager.createQuery("from User", User.class).getResultList();
     }
 
-    @Transactional(readOnly = true)
     @Override
     public User getUserById(Long id) {
         return entityManager.find(User.class, id);
     }
 
-    @Transactional
     @Override
     public void createUser(User user) {
         entityManager.persist(user);
     }
 
-    @Transactional
     @Override
     public void updateUser(User user) {
         entityManager.merge(user);
     }
 
-    @Transactional
     @Override
     public void deleteUser(Long id) {
         User userToDeleted = getUserById(id);
